4fc325a30761632027ab8cc0e3917284d01b3d25
shijbian <shijiabian@gmail.com>
2017-03-23 22:05:50 -0400

    hope

--- tests/testthat/testFirstPackage.R
+++ tests/testthat/testFirstPackage.R
@@ -1,10 +1,6 @@
 library(firstPackage)
 library(testthat)
 
-test_that("fars_read is able to read the file", {
-   expect_equal(dim(fars_read("./inst/extData/accident_2014.csv"))[1], 30056)
- })
-
 test_that("make_filename can make up the name", {
   expect_equal(make_filename(20), "accident_20.csv")
 })
